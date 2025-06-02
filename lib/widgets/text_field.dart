cb064788bfe241ab4f68f143b7f9a094af890a31
Youssef <joeace2002@gmail.com>
2025-06-02 12:29:21 +0100

    overall improvement and adding features

--- lib/widgets/text_field.dart
+++ lib/widgets/text_field.dart
@@ -5,8 +5,17 @@ class MyTextField extends StatelessWidget {
   // final Function onTap;
   final String labelText;
   final Icon? icon;
-
-  const MyTextField({super.key, required this.labelText, this.icon});
+  final bool isEmail;
+  final bool isPassword;
+  final Function onChange;
+  const MyTextField({
+    super.key,
+    required this.labelText,
+    this.icon,
+    this.isEmail = false,
+    this.isPassword = false,
+    required this.onChange,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +24,8 @@ class MyTextField extends StatelessWidget {
       duration: Duration(milliseconds: 800),
       child: TextField(
         cursorColor: Colors.black,
+        keyboardType: isEmail ? TextInputType.emailAddress : null,
+        obscureText: isPassword,
         decoration: InputDecoration(
           prefixIcon: icon ?? null,
           labelText: labelText,
@@ -23,6 +34,7 @@ class MyTextField extends StatelessWidget {
             borderSide: BorderSide(color: Colors.black),
           ),
         ),
+        onChanged: (value) => onChange(value),
       ),
     );
   }
