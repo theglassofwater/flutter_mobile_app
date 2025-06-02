9582ec2833524aeda666476446aac35b17b3bfa5
Youssef <joeace2002@gmail.com>
2025-06-02 12:26:24 +0100

    revamped how caching/provider works

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobile_app/screens/onboarding.dart';
-import 'package:flutter_mobile_app/services/location_storage.dart';
 import 'package:flutter_mobile_app/services/overpass_api.dart';
-import 'package:flutter_mobile_app/services/theme_storage.dart';
+import 'package:flutter_mobile_app/utils/account_provider.dart';
 import 'package:flutter_mobile_app/utils/location_provider.dart';
 import 'package:flutter_mobile_app/utils/theme_provider.dart';
-import 'package:flutter_mobile_app/big_widgets/bottom_navbar.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final isDark = await ThemeStorage().isDark();
-
   final prefs = await SharedPreferences.getInstance();
-
   final OverpassApi overpassApi = OverpassApi();
+
+  final themeProvider = ThemeProvider(prefs);
+  final accountProvider = AccountProvider(prefs);
+  final locationProvider = LocationProvider(prefs, overpassApi);
+  locationProvider.init();
+
   runApp(
-    // ChangeNotifierProvider(
-    //   create: (context) => ThemeProvider(isDark),
-    //   child: const MyApp(),
-    // ),
     MultiProvider(
       providers: [
         Provider<OverpassApi>(create: (_) => overpassApi),
-        ChangeNotifierProvider(create: (context) => ThemeProvider(isDark)),
-        ChangeNotifierProvider(
-          create: (context) => LocationProvider(prefs, overpassApi),
-        ),
+        ChangeNotifierProvider(create: (context) => accountProvider),
+        ChangeNotifierProvider(create: (context) => themeProvider),
+        ChangeNotifierProvider(create: (context) => locationProvider),
       ],
       child: MyApp(),
     ),
