03a4f3951d37d4257337629936181035e40e15f4
Youssef <joeace2002@gmail.com>
2025-06-02 12:28:22 +0100

    revamped forms, and changed variable names

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_mobile_app/big_widgets/email_verification_form.dart';
-import 'package:flutter_mobile_app/models/Address.dart';
-import 'package:flutter_mobile_app/models/Location.dart';
 import 'package:flutter_mobile_app/models/POI.dart';
 import 'package:flutter_mobile_app/screens/login.dart';
-import 'package:flutter_mobile_app/services/overpass_api.dart';
 import 'package:flutter_mobile_app/styles/colors.dart';
 import 'package:flutter_mobile_app/big_widgets/bottom_navbar.dart';
 import 'package:flutter_mobile_app/big_widgets/location_selection_form.dart';
 import 'package:flutter_mobile_app/big_widgets/sign_up_form.dart';
+import 'package:flutter_mobile_app/utils/account_provider.dart';
+import 'package:flutter_mobile_app/utils/location_provider.dart';
 import 'package:flutter_mobile_app/utils/screen_transitions.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:flutter_mobile_app/models/POI.dart';
+import 'package:provider/provider.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({super.key});
@@ -41,25 +39,80 @@ class _SignUpPageState extends State<SignUpPage> {
 
   // Locations
 
-  List<POI> _pois = [];
-  POI? homeStation = null;
-  POI? workStation = null;
+  List<POI> pois = [];
+  POI? home;
+  POI? work;
 
   // Switching to Login
   void switchToLogin() {
     slideFrom(context, Login(), replace: true, fromLeft: true);
-    // Navigator.pushReplacement(
-    //   context,
-    //   MaterialPageRoute(builder: (_) => Login()),
-    // );
   }
 
   // Proccessing Sign Up
-  void onSignUp() {
+  void onSignUp(
+    String? name,
+    String? email,
+    String? password, {
+    bool toApp = false,
+  }) {
+    if (name != null) {
+      Provider.of<AccountProvider>(context, listen: false).setName(name);
+    } else {
+      print("name not set");
+    }
+
+    if (email != null) {
+      Provider.of<AccountProvider>(context, listen: false).setEmail(email);
+    } else {
+      print("email not set");
+    }
+
+    if (password != null) {
+      Provider.of<AccountProvider>(
+        context,
+        listen: false,
+      ).setPassword(password);
+    } else {
+      print("password not set");
+    }
+    if (toApp) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => BottomNavBar()),
+      );
+    }
+    controller.animateToPage(
+      1, // target page index
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut, // this is your animation style
+    );
+    setStep(1);
+  }
+
+  void onEmailVerification(String code) {
+    controller.animateToPage(
+      2, // target page index
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut, // this is your animation style
+    );
+    setStep(2);
+  }
+
+  void onLocationSelection(POI? home, POI? work, bool isTrainUser) {
+    if (home != null) {
+      Provider.of<LocationProvider>(context, listen: false).setHome(home);
+    }
+    if (work != null) {
+      Provider.of<LocationProvider>(context, listen: false).setWork(work);
+    }
+    print(home);
+    print(work);
+    // if home and work is not not, or isTrainUser=false then...
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (_) => BottomNavBar()),
     );
+    // widget.onSignUp();
   }
 
   void setStep(int step) {
@@ -90,13 +143,13 @@ class _SignUpPageState extends State<SignUpPage> {
           EmailVerificationForm(
             controller: controller,
             setStep: setStep,
-            onSubmit: () => {},
+            onSubmit: onEmailVerification,
           ),
-          LocationSelection(
+          LocationSelectionForm(
             mapController: _mapController,
             controller: controller,
             setStep: setStep,
-            onSignUp: onSignUp,
+            onLocationSelection: onLocationSelection,
           ),
         ],
       ),
